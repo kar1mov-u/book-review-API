fe651137a4b5e188c97c83f8a3f75ba6039b18de
usmon <usmonkarimov0726@gmail.com>
2024-11-18 09:17:24 +0500

    Configured DB

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
-
+from .database import create_db_and_tables
 app = FastAPI()
 
 
+
+@app.on_event("startup")
+def  on_startup():
+    create_db_and_tables()
+    
+    
+@app.get('/check')
+def check():
+    return {"data":"working"}
